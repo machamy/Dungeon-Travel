93d48f59c11feaa23f6300715fa0b5b83322c465
leejhee <81043928+leejhee@users.noreply.github.com>
2024-01-15 15:17:31 +0900

    던전 구현(1)
    1. 카메라 시점 쿼터뷰로 돌림
    2. 카메라가 플레이어 따라가게만 해놓음
    3. 디코에서 두번째 에셋으로, 배치도대로 바닥만 깜
    4. 플레이어 캡슐로 더미만 만들고, 먼저 키보드로 이동 가능. 패드는 없어서 따로 해보긴 해야함.

--- Assets/Scripts/Game/Dungeon/PlayerUnit.cs
+++ Assets/Scripts/Game/Dungeon/PlayerUnit.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerUnit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float speed;
+
+    Rigidbody rigid;
+    Vector3 moveVec;
+
+    void Awake()
     {
-        
+        rigid = GetComponentInChildren<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerMove();
+
+        playerTurn();
+    }
+
+    void playerMove()
+    {
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+
+    void playerTurn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    // Input System에서 사용. 패드,키보드 등의 입력을 vector2로 받아온다.
+    void OnMove(InputValue value)
+    {
+        Vector2 inputVec = value.Get<Vector2>(); //이미 normalized된 녀석.
+        moveVec = new Vector3(inputVec.x, 0, inputVec.y);
     }
 }
