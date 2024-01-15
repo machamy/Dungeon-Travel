0bd74edc3df4e00327d887feae38ac13c2a66af7
machamy <macham2001@gmail.com>
2024-01-16 00:55:25 +0900

    상호작용 가능 유닛 표시
    상호작용 가능한 오브젝트일 경우 해당 오브젝트 붉은 윤곽선으로 표시함
    
    1. BaseInteractionUnit :  앞으로 상호작용 오브젝트들이 파생될 클래스
    2. PlayerUnit : 매 틱(LateUpdate)마다 raycast 후 처음 부딪힌 상호작용 대상과 작용함
    2-1. 현재 다른 벽도 뚫고 지나가므로 후에 벽 추가시 tag와 같은 요소 확인 코드 추가 필요
    3. 윤곽선 셰이더 추가함

--- Assets/Scripts/Game/Dungeon/PlayerUnit.cs
+++ Assets/Scripts/Game/Dungeon/PlayerUnit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerUnit : MonoBehaviour
 {
-    [SerializeField] float speed;
+    [SerializeField] protected float speed;
+    [SerializeField] protected float interactionRange;
 
     Rigidbody rigid;
     Vector3 moveVec;
 
+
     void Awake()
     {
         rigid = GetComponentInChildren<Rigidbody>();
+        
+    }
+
+    private void Start()
+    {
+        focusUnit = null;
     }
 
     // Update is called once per frame
@@ -23,9 +32,14 @@ public class PlayerUnit : MonoBehaviour
         playerTurn();
     }
 
+    private void LateUpdate()
+    {
+        CheckInteraction();
+    }
+
     void playerMove()
     {
-        transform.position += moveVec * speed * Time.deltaTime;
+        transform.position += moveVec * (speed * Time.deltaTime);
     }
 
     void playerTurn()
@@ -38,5 +52,68 @@ public class PlayerUnit : MonoBehaviour
     {
         Vector2 inputVec = value.Get<Vector2>(); //이미 normalized된 녀석.
         moveVec = new Vector3(inputVec.x, 0, inputVec.y);
+    }
+
+    void OnUseEvent()
+    {
+        
+    }
+
+    /// <summary>
+    /// 상호작용 가능한 유닛을 focusUnit에 등록
+    /// </summary>
+    void CheckInteraction()
+    {
+        Ray r = new Ray(transform.position, transform.forward);
+        RaycastHit[] hiArr = Physics.RaycastAll(r, interactionRange);
+        bool found = false;
+        if (hiArr.Length != 0)
+        {
+            foreach (var hi in hiArr)
+            {
+                //TODO: 벽이나 기타 장애물 확인 코드 필요 (tag 등 이용)
+                if (hi.collider.gameObject.TryGetComponent(out BaseInteractionUnit iu))
+                {
+                    FocusUnit = iu;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if(!found)
+            FocusUnit = null;
+        
+        
+        //디버그용 그리기
+        Debug.DrawRay(r.origin,r.direction*(interactionRange*1), Color.green);
+    }
+
+    private BaseInteractionUnit focusUnit;
+
+    /// <summary>
+    /// 포커스 등록/해제 관리용 프라퍼티
+    /// </summary>
+    public BaseInteractionUnit FocusUnit
+    {
+        get => focusUnit;
+        set
+        {
+            // 기존과 같으면 무시
+            if (focusUnit == value)
+                return;
+            
+            // 기존 포커스 없을 시
+            if (focusUnit is not null)
+                focusUnit.IsFocused = false;
+            
+            focusUnit = value;
+            
+            // 포커스 해제시(변경 X)
+            if (value is null)
+                return;
+            
+            // 포커스 변경시
+            focusUnit.IsFocused = true;
+        }
     }
 }
