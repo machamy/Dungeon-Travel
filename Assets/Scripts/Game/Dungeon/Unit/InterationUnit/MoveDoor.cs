80e4eb852ebd15facc6ca0bddb517c10d21e2215
machamy <macham2001@gmail.com>
2024-01-17 20:39:54 +0900

    움직여서 열리는 문

---
+++ Assets/Scripts/Game/Dungeon/Unit/InterationUnit/MoveDoor.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Scripts.Game.Dungeon.Unit
+{
+    public class MoveDoor : BaseInteractionUnit,IDoor, IButtonListener
+    {
+        public Vector3 Direction;
+        public float moveScale;
+        public float time;
+        public int frame;
+
+        public bool isOpened;
+
+        public void Open()
+        {
+            Vector3 destination = Direction.normalized * (moveScale);
+            destination.Scale(transform.localScale);
+            StartCoroutine(Move(transform.position+destination,time,frame));
+            isOpened = true;
+        }
+
+        public void Close()
+        {
+            Vector3 destination = Direction.normalized * (moveScale);
+            destination.Scale(transform.localScale);
+            StartCoroutine(Move(transform.position-destination,time,frame));
+            isOpened = false;
+        }
+
+        private IEnumerator Move(Vector3 destination, float time, int count = 16)
+        {
+            var delay = new WaitForSeconds(time/count);
+            Vector3 deltaPos = destination - transform.position;
+            
+            for (int i = 0; i < 16; i++)
+            {
+                transform.Translate(deltaPos/count);
+                yield return delay;
+            }
+
+            transform.position = destination;
+        }
+
+        public void Do()
+        {
+            if(!isOpened)
+                Open();
+        }
+
+        public void Toggle()
+        {
+            if (isOpened)
+                Close();
+            else
+                Open();
+        }
+    }
+}
