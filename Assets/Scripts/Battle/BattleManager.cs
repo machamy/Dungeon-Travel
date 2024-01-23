db68bce9369b1f009959aa0635cd988b1624dfc1
jinwoo jung <alamash@naver.com>
2024-01-23 15:35:52 +0900

    적 프리펩 불러오기

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -16,10 +16,10 @@ public class BattleManager : MonoBehaviour
             // 없을경우 생성
             if (instance == null)
             {
-                GameObject bm = GameObject.Find("@Battle");
+                GameObject bm = GameObject.Find("BattleSystem");
                 if (bm == null)
                 {
-                    bm = new GameObject("@Battle");
+                    bm = new GameObject("BattleSystem");
                 }
 
                 instance = bm.AddComponent<BattleManager>();
@@ -32,34 +32,33 @@ public class BattleManager : MonoBehaviour
     public enum TurnState { START, PROCESSING, END } // 턴 상태 열거형
     public bool IsAttacked { get; set; } // 나중에 던전쪽으로 빼는게 나을듯
 
-    public bool isProcessing = false; // processing 상태를 알려주는 변수
+    private bool isProcessing = false; // processing 상태를 알려주는 변수
 
     public BattleState bState { get; set; }
     public TurnState tState { get; set; }
 
-    public BattleState State;
-
     public GameObject[] playerStation;
-
     private GameObject[] playerGO = new GameObject[5];
     public GameObject[] playerPrefab;
-
     private HUDmanager[] playerHUD = new HUDmanager[6];
 
+    private Transform[] EnemySpawnerPoints = new Transform[4]; // 적 스폰지점 위치 받아오는 변수
+    int SpawnCount; // 스폰장소 지정 변수
     private Unit[] playerunit = new Unit[6], enemyunit = new Unit[6];
     private void Awake()
     {
         bState = BattleState.INBATTLE; // 
         tState = TurnState.START;
         isProcessing = false;
-
+        SpawnCount = 0;
         SetupBattle();
     }
 
 
     private void SetupBattle()
     {
-        for (int i = 0; i < playerPrefab.Length; i++) //플레이어 프리펩 불러오기
+        //플레이어 프리펩 불러오기
+        for (int i = 0; i < playerPrefab.Length; i++) 
         {
             GameObject tempplayerGO = Instantiate(playerPrefab[i], playerStation[i].transform);  //플레이어 프리펩 생성
             Unit tempplayerunit = tempplayerGO.GetComponent<Unit>();
@@ -73,8 +72,27 @@ public class BattleManager : MonoBehaviour
             playerunit[i].ConnectHUD(playerStation[position].GetComponent<HUDmanager>());  //유닛스크립트에 HUD 매니저 연결
             Debug.Log("Player" + i + "세팅 완료");
         }
-        // 플레이어, 적 프리펩 불러오기 등등(미구현)
+        // 적 프리펩 불러오기
+        for(int i = 0; i < EnemySpawnerPoints.Length; i++) // 적 스폰 위치 받아오기
+        {
+            EnemySpawnerPoints[i] = GameObject.Find("EnemySpawner" + i).GetComponent<Transform>();
+        }
+        EnemySpawn(Define_Battle.Enemy_Type.Rabbit); // 적 스폰은 나중에 데이터로 처리할수 있게 변경 예정
         StartCoroutine(BattleSequenceCor());
+    }
+    private void EnemySpawn(Define_Battle.Enemy_Type enemy_Type) // 적 스폰하는 함수 프리펩으로 받아와서 생성
+    {
+        GameObject enemy;
+        try
+        {
+            enemy = Instantiate(Resources.Load<GameObject>($"BattlePrefabs/EnemyPrefabs/{enemy_Type}"));
+            enemy.transform.position = EnemySpawnerPoints[SpawnCount].position;
+            enemy.transform.parent = EnemySpawnerPoints[SpawnCount++].transform;
+        }
+        catch
+        {
+            Debug.LogError($"Load 문제 발생 BattlePrefabs/EnemyPrefabs/{enemy_Type}확인 바람");
+        }
     }
 
     private void Update()
