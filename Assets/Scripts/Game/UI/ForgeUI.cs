88f72b27ac70431ae9988a7f7cece3715b2a94de
yuchan <rladbcks56@naver.com>
2024-01-23 22:43:32 +0900

    대장간 UI 기능 추가

--- Assets/Scripts/Game/UI/ForgeUI.cs
+++ Assets/Scripts/Game/UI/ForgeUI.cs
@@ -11,45 +11,65 @@ using UnityEngine.UI;
 public class ForgeUI : MonoBehaviour
 {
     public GameObject behaviourContainer, typeContainer, listContainer, infoContainer;
-    public GameObject askBuyPanel;
+    public GameObject askBuyPanel, askExitPanel, talkPanel, bottomPanel;
     public GameObject itemNameText;
-    public GameObject behaviourFirstSelect, weaponFirstSelect, askBuyFirstSelect;
+    public GameObject buttonDescriptionText;
+    public GameObject talkText;
+    public GameObject behaviourFirstSelect, weaponFirstSelect, askBuyFirstSelect, askExitFirstSelect;
     public PlayerInput playerInput;
     public GameObject[] typeButton = new GameObject[5];
-    string currentState = "SelectBehaviour";
+    private State currentState = State.selectBehaviour;
     int currentType = 0;
+    string selectButtonName;
 
     public InputActionReference mainNavigation, yNavigation;
 
+    public enum State
+    {
+        selectBehaviour,
+        selectWeapon,
+        askBuyItem,
+        Talk,
+        askExit
+    }
 
     public void SelectBehaviour()
     {
-        currentState = "SelectBehaviour";
+        currentState = State.selectBehaviour;
+
         behaviourContainer.SetActive(true);
+        bottomPanel.SetActive(true);
+        askExitPanel.SetActive(false);
         typeContainer.SetActive(false);
         listContainer.SetActive(false);
         infoContainer.SetActive(false);
+        talkPanel.SetActive(false);
+
         SelectButton(behaviourFirstSelect);
         SelectNavigate(mainNavigation);
     }
 
     public void SelectWeapon()
     {
-        currentState = "SelectWeapon";
-        behaviourContainer.SetActive(false);
-        askBuyPanel.SetActive(false);
+        currentState = State.selectWeapon;
+
         typeContainer.SetActive(true);
         listContainer.SetActive(true);
         infoContainer.SetActive(true);
+        behaviourContainer.SetActive(false);
+        askBuyPanel.SetActive(false);
+
         SelectButton(weaponFirstSelect);
         SelectNavigate(yNavigation);
     }
 
     public void OnSwitchType(InputValue value)
     {
-        if (currentState != "SelectWeapon") return;
+        if (currentState != State.selectWeapon) return;
+
         int intvalue = (int)value.Get<float>();
         if (currentType + intvalue < 0 || currentType + intvalue > 4) return;
+
         SelectButton(weaponFirstSelect);
         typeButton[currentType].GetComponent<Image>().color = new Color(0, 1, 1, 0.5f);
         typeButton[currentType += intvalue].GetComponent<Image>().color = new Color(1, 1, 0, 0.5f);
@@ -65,10 +85,36 @@ public class ForgeUI : MonoBehaviour
 
     public void AskBuyItem(string itemName)
     {
-        currentState = "AskBuyItem";
+        currentState = State.askBuyItem;
+
         askBuyPanel.SetActive(true);
+
         itemNameText.GetComponent<TextMeshProUGUI>().text = "You wanna buy " + itemName + "?";
-        StartCoroutine(WaitForSelectButton(0.000001f));
+        SelectButton(askBuyFirstSelect);
+        SelectNavigate(mainNavigation);
+    }
+
+    public void Talk()
+    {
+        currentState = State.Talk;
+
+        talkPanel.SetActive(true);
+        bottomPanel.SetActive(false);
+        behaviourContainer.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        int textNum = Random.Range(0, UIDB.talkList.Count);
+        talkText.GetComponent<TextMeshProUGUI>().text = UIDB.talkList[textNum];
+    }
+
+    public void AskExit()
+    {
+        currentState = State.askExit;
+
+        askExitPanel.SetActive(true);
+
+        SelectButton(askExitFirstSelect);
         SelectNavigate(mainNavigation);
     }
 
@@ -76,25 +122,58 @@ public class ForgeUI : MonoBehaviour
     {
         switch (currentState)
         {
-            case "AskBuyItem":
-                SelectWeapon();
-                break;
-            case "SelectWeapon":
-                SelectBehaviour();
-                break;
+            case State.askBuyItem:
+                SelectWeapon(); break;
+            case State.selectWeapon:
+                SelectBehaviour(); break;
+            case State.selectBehaviour:
+                AskExit(); break;
+            case State.Talk:
+                SelectBehaviour(); break;
+            case State.askExit:
+                SelectBehaviour(); break;
         }
     }
 
+    public void OnSubmit()
+    {
+        switch (currentState)
+        {
+            case State.Talk:
+                SelectBehaviour(); break;
+        }
+    }
+
+    public void OnClick()
+    {
+        switch (currentState)
+        {
+            case State.Talk:
+                SelectBehaviour(); break;
+        }
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject != null)
+            selectButtonName = EventSystem.current.currentSelectedGameObject.name;
+
+        if (UIDB.buttonDescription.ContainsKey(selectButtonName))
+            buttonDescriptionText.GetComponent<TextMeshProUGUI>().text =
+                UIDB.buttonDescription[selectButtonName];
+    }
+
     public void SelectButton(GameObject button) =>
+        StartCoroutine(WaitForSelectButton(button));
+
+    private IEnumerator WaitForSelectButton(GameObject button)
+    {
+        yield return new WaitForSeconds(0.001f);
         EventSystem.current.SetSelectedGameObject(button);
+    }
 
     public void SelectNavigate(InputActionReference navigation) =>
         ((InputSystemUIInputModule)EventSystem.current.currentInputModule).move = navigation;
 
-    private IEnumerator WaitForSelectButton(float time)
-    {
-        yield return new WaitForSeconds(time);
-        SelectButton(askBuyFirstSelect);
-    }
 
 }
